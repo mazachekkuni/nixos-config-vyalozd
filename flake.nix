fa2e1c0e4271bbfb98aec3668dd0f9a3cc8f57d1
mazachekkuni <romanklovanin@gmail.com>
2026-06-11 17:12:34 +0300

    vyalozd

--- flake.nix
+++ flake.nix
@@ -30,9 +30,9 @@
 
   outputs = { self, nixpkgs, home-manager, spicetify-nix, niri, ... }@inputs: {
     nixosConfigurations.awesomebox = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       specialArgs = { inherit inputs; }; 
       modules = [
+	{ nixpkgs.hostPlatform = "x86_64-linux"; } 
         ./hardware-configuration.nix
         ./configuration.nix
         ./noctalia.nix  
