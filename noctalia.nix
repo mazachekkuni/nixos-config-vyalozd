0be782873bdbe2480af2644615b8cf0310988060
mazachekkuni <romanklovanin@gmail.com>
2026-06-16 18:42:04 +0300

    vyalozd

--- noctalia.nix
+++ noctalia.nix
@@ -1,12 +1,10 @@
 { pkgs, inputs, ... }:
 
 {
-  # Додаємо пакет Noctalia з головного флейка у систему
   environment.systemPackages = [
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
-  # Вмикаємо обов'язкові сервіси для роботи системних toggles (Wi-Fi, батарея, Bluetooth)
   services.upower.enable = true;
   networking.networkmanager.enable = true;
   hardware.bluetooth.enable = true;
